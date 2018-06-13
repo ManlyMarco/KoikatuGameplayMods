35a4f58a18056b158756dff0a7677829e22eb7e0
MarC0 <dummyaccx@gmail.com>
2018-06-13 17:33:31 +0200

    Added dependancy to config manager

--- KoikatuGameplayMod/KoikatuGameplayMod.cs
+++ KoikatuGameplayMod/KoikatuGameplayMod.cs
@@ -7,8 +7,22 @@ using BepInEx;
 namespace KoikatuGameplayMod
 {
     [BepInPlugin("KoikatuGameplayMod", "Koikatu Gameplay Tweaks and Improvements", "1.0")]
+    [BepInDependency("com.bepis.bepinex.configurationmanager", BepInDependency.DependencyFlags.SoftDependency)]
     public class KoikatuGameplayMod : BaseUnityPlugin
     {
+        private void Update()
+        {
 
+        }
+
+        private void Start()
+        {
+
+        }
+
+        private void OnGUI()
+        {
+
+        }
     }
 }
