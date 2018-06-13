f67cc0beca31e04664df3861bbaa275df052d05b
MarC0 <dummyaccx@gmail.com>
2018-06-13 17:30:22 +0200

    Created plugin project

---
+++ KoikatuGameplayMod/KoikatuGameplayMod.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BepInEx;
+
+namespace KoikatuGameplayMod
+{
+    [BepInPlugin("KoikatuGameplayMod", "Koikatu Gameplay Tweaks and Improvements", "1.0")]
+    public class KoikatuGameplayMod : BaseUnityPlugin
+    {
+
+    }
+}
