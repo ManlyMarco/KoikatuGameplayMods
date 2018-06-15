dae830c59a7f72fd2ddd480708a14eaf3e773e33
MarC0 <dummyaccx@gmail.com>
2018-06-15 17:01:55 +0200

    Added ability to force raw with blue option on some conditions

---
+++ KoikatuGameplayMod/Hooks.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Harmony;
+using UnityEngine;
+using Random = System.Random;
+
+namespace KoikatuGameplayMod
+{
+    static class Hooks
+    {
+        private static readonly Random RandomGen = new Random();
+
+        public static void ApplyHooks()
+        {
+            var i = HarmonyInstance.Create("marco-gameplaymod");
+            i.PatchAll(typeof(Hooks));
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(HSprite), nameof(HSprite.OnInsertNoVoiceClick), new Type[] { })]
+        public static void IsNamaInsertOkPre(HSprite __instance)
+        {
+            if (!Input.GetMouseButtonUp(0) || !__instance.IsSpriteAciotn())
+                return;
+
+            if (__instance.flags.isInsertOK)
+                return;
+
+            // Check if player can force raw 
+            var girlOrgasms = __instance.flags.count.sonyuOrg;
+            // OUT_A is resting after cumming outside
+            if (__instance.flags.nowAnimStateName == "OUT_A" || __instance.flags.isDenialvoiceWait || girlOrgasms >= 3 - RandomGen.Next(0, 3))
+            {
+                // Make girl angry
+                __instance.flags.lstHeroine[0].anger = Math.Min(100, __instance.flags.lstHeroine[0].anger + 34);
+                __instance.flags.lstHeroine[0].isAnger = true;
+                __instance.flags.lstHeroine[0].favor = Math.Max(0, __instance.flags.lstHeroine[0].favor - 10);
+
+                __instance.flags.lstHeroine[0].chaCtrl.tearsLv = 1;
+                __instance.flags.lstHeroine[0].chaCtrl.ChangeEyesShaking(true);
+                __instance.flags.lstHeroine[0].chaCtrl.ChangeLookEyesTarget(2);
+                __instance.flags.lstHeroine[0].chaCtrl.ChangeTongueState(0);
+
+                // Trick game into allowing raw
+                __instance.flags.isDebug = true;
+                __instance.flags.isDenialvoiceWait = false;
+            }
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(HSprite), nameof(HSprite.OnInsertNoVoiceClick), new Type[] { })]
+        public static void IsNamaInsertOkPost(HSprite __instance)
+        {
+            __instance.flags.isDebug = false;
+        }
+    }
+}
--- KoikatuGameplayMod/KoikatuGameplayMod.cs
+++ KoikatuGameplayMod/KoikatuGameplayMod.cs
@@ -1,28 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using BepInEx;
 
 namespace KoikatuGameplayMod
 {
     [BepInPlugin("KoikatuGameplayMod", "Koikatu Gameplay Tweaks and Improvements", "1.0")]
-    [BepInDependency("com.bepis.bepinex.configurationmanager", BepInDependency.DependencyFlags.SoftDependency)]
+    [BepInDependency("marco-gameplaymod", BepInDependency.DependencyFlags.SoftDependency)]
     public class KoikatuGameplayMod : BaseUnityPlugin
     {
-        private void Update()
+        public KoikatuGameplayMod()
+        {
+            Hooks.ApplyHooks();
+        }
+
+        /*private void Update()
         {
 
         }
 
         private void Start()
         {
-
         }
 
         private void OnGUI()
         {
 
-        }
+        }*/
     }
 }
