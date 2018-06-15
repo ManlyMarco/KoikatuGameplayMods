5f48e1394427161aeeeac48a1f158b2e13f6dfcd
MarC0 <dummyaccx@gmail.com>
2018-06-15 17:28:21 +0200

    Added more options to force raw

--- KoikatuGameplayMod/Hooks.cs
+++ KoikatuGameplayMod/Hooks.cs
@@ -3,9 +3,11 @@ using Harmony;
 using UnityEngine;
 using Random = System.Random;
 
+// ReSharper disable InconsistentNaming
+
 namespace KoikatuGameplayMod
 {
-    static class Hooks
+    internal static class Hooks
     {
         private static readonly Random RandomGen = new Random();
 
@@ -15,9 +17,11 @@ namespace KoikatuGameplayMod
             i.PatchAll(typeof(Hooks));
         }
 
+        #region ForceRaw
+
         [HarmonyPrefix]
         [HarmonyPatch(typeof(HSprite), nameof(HSprite.OnInsertNoVoiceClick), new Type[] { })]
-        public static void IsNamaInsertOkPre(HSprite __instance)
+        public static void OnInsertNoVoiceClickPre(HSprite __instance)
         {
             if (!Input.GetMouseButtonUp(0) || !__instance.IsSpriteAciotn())
                 return;
@@ -25,32 +29,76 @@ namespace KoikatuGameplayMod
             if (__instance.flags.isInsertOK)
                 return;
 
-            // Check if player can force raw 
+            var heroine = __instance.flags.lstHeroine[0];
             var girlOrgasms = __instance.flags.count.sonyuOrg;
-            // OUT_A is resting after cumming outside
-            if (__instance.flags.nowAnimStateName == "OUT_A" || __instance.flags.isDenialvoiceWait || girlOrgasms >= 3 - RandomGen.Next(0, 3))
+
+            // Check if player can force raw 
+            // OUT_A is resting after popping the cork outdoors
+            if (__instance.flags.nowAnimStateName == "OUT_A" ||
+                __instance.flags.isDenialvoiceWait ||
+                girlOrgasms >= 3 + RandomGen.Next(0, 3) - heroine.lewdness / 66)
             {
-                // Make girl angry
-                __instance.flags.lstHeroine[0].anger = Math.Min(100, __instance.flags.lstHeroine[0].anger + 34);
-                __instance.flags.lstHeroine[0].isAnger = true;
-                __instance.flags.lstHeroine[0].favor = Math.Max(0, __instance.flags.lstHeroine[0].favor - 10);
+                MakeGirlAngry(heroine);
 
-                __instance.flags.lstHeroine[0].chaCtrl.tearsLv = 1;
-                __instance.flags.lstHeroine[0].chaCtrl.ChangeEyesShaking(true);
-                __instance.flags.lstHeroine[0].chaCtrl.ChangeLookEyesTarget(2);
-                __instance.flags.lstHeroine[0].chaCtrl.ChangeTongueState(0);
-
-                // Trick game into allowing raw
-                __instance.flags.isDebug = true;
+                ForceAllowRaw(__instance);
                 __instance.flags.isDenialvoiceWait = false;
             }
         }
 
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(HSprite), nameof(HSprite.OnInsertClick), new Type[] { })]
+        public static void OnInsertClickPre(HSprite __instance)
+        {
+            if (!Input.GetMouseButtonUp(0) || !__instance.IsSpriteAciotn())
+                return;
+
+            if (__instance.flags.isInsertOK)
+                return;
+
+            var heroine = __instance.flags.lstHeroine[0];
+            var girlOrgasms = __instance.flags.count.sonyuOrg;
+
+            // Check if girl allows raw
+            if (girlOrgasms >= 4 + RandomGen.Next(0, 3) - heroine.lewdness / 45)
+                ForceAllowRaw(__instance);
+        }
+
         [HarmonyPostfix]
         [HarmonyPatch(typeof(HSprite), nameof(HSprite.OnInsertNoVoiceClick), new Type[] { })]
-        public static void IsNamaInsertOkPost(HSprite __instance)
+        public static void OnInsertNoVoiceClickPost(HSprite __instance)
+        {
+            ResetForceAllowRaw(__instance);
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(HSprite), nameof(HSprite.OnInsertClick), new Type[] { })]
+        public static void OnInsertClickPost(HSprite __instance)
+        {
+            ResetForceAllowRaw(__instance);
+        }
+
+        private static void ForceAllowRaw(HSprite instance)
+        {
+            instance.flags.isDebug = true;
+        }
+
+        private static void ResetForceAllowRaw(HSprite __instance)
         {
             __instance.flags.isDebug = false;
         }
+
+        private static void MakeGirlAngry(SaveData.Heroine heroine)
+        {
+            heroine.anger = Math.Min(100, heroine.anger + 34);
+            heroine.isAnger = true;
+            heroine.favor = Math.Max(0, heroine.favor - 10);
+
+            heroine.chaCtrl.tearsLv = 1;
+            heroine.chaCtrl.ChangeEyesShaking(true);
+            heroine.chaCtrl.ChangeLookEyesTarget(2);
+            heroine.chaCtrl.ChangeTongueState(0);
+        }
+
+        #endregion
     }
 }
