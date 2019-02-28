c369d4b67abd9a74b363ca89f725b25984f73823
MarC0 <dummyaccx@gmail.com>
2019-02-28 21:20:43 +0100

    Added exit button to virgin H scenes, girl will keep "First time" status if no penetration was done

--- KoikatuGameplayMod/Hooks.cs
+++ KoikatuGameplayMod/Hooks.cs
@@ -28,6 +28,64 @@ namespace KoikatuGameplayMod
             i.Patch(m, null, null, new HarmonyMethod(typeof(Hooks), nameof(NPCLoadAllUnlock)));
         }
 
+        #region ExitFirstH
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(HSceneProc), "NewHeroineEndProc")]
+        public static void NewHeroineEndProcPost(HSceneProc __instance)
+        {
+            OnHEnd(__instance);
+        }
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(HSceneProc), "EndProc")]
+        public static void EndProcPost(HSceneProc __instance)
+        {
+            OnHEnd(__instance);
+        }
+
+        private static void OnHEnd(HSceneProc proc)
+        {
+            // If girl is stil a virgin, keep her first time status
+            foreach (var heroine in proc.flags.lstHeroine)
+            {
+                if (heroine.isVirgin && heroine.isAnalVirgin)
+                    heroine.hCount = 0;
+            }
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(HSceneProc), "Start")]
+        public static void HSpriteUpdatePre(HSceneProc __instance)
+        {
+            // Adjust help sprite location so it doesn't cover the back button
+            var rt = __instance.sprite.objFirstHHelpBase.transform.parent.GetComponent<RectTransform>();
+            rt.anchorMin = new Vector2(0.04f, 0f);
+            rt.offsetMax = Vector2.zero;
+            rt.offsetMin = Vector2.zero;
+        }
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(HSprite), "Update")]
+        public static void HSpriteUpdatePre(HSprite __instance, out bool __state)
+        {
+            // Skip code that hides the back button
+            __state = false;
+            if (__instance.flags.lstHeroine.Count != 0 && __instance.flags.lstHeroine[0].hCount == 0)
+            {
+                __state = true;
+                __instance.flags.lstHeroine[0].hCount = 1;
+            }
+        }
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(HSprite), "Update")]
+        public static void HSpriteUpdatePost(HSprite __instance, bool __state)
+        {
+            // Restore original hcount
+            if (__state)
+                __instance.flags.lstHeroine[0].hCount = 0;
+        }
+
+        #endregion
+
         #region Chara limit unlock
 
         public static IEnumerable<CodeInstruction> NPCLoadAllUnlock(IEnumerable<CodeInstruction> instructions)
@@ -80,7 +138,7 @@ namespace KoikatuGameplayMod
             });
         }
 
-        #region GirlEroLevel
+        #region Modify girl lewd level
 
         private static void UpdateGirlLewdness(HSprite __instance)
         {
