ac350fc2dd36cc532f0bfd2e9569a4bea0a69736
MarC0 <dummyaccx@gmail.com>
2019-04-06 20:52:24 +0200

    Fix NightDarkener affecting main game, only works in free h on selected maps now; Version up to 1.1

--- KK_NightDarkener/NightDarkener.cs
+++ KK_NightDarkener/NightDarkener.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using BepInEx;
 using Harmony;
 using UnityEngine;
@@ -12,9 +13,7 @@ namespace KK_NightDarkener
     public class NightDarkener : BaseUnityPlugin
     {
         public const string GUID = "Marco.NightDarkener";
-        internal const string Version = "1.0";
-
-        private static readonly HashSet<SunLightInfo> AdjustedList = new HashSet<SunLightInfo>();
+        internal const string Version = "1.1";
         
         [DisplayName("Enable dark fog at night")]
         [Description("Horror game effect.\nChanges take effect next time you load a night map.")]
@@ -24,50 +23,47 @@ namespace KK_NightDarkener
         [Description("The lower the exposure, the darker the game will be.\nChanges take effect next time you load a night map.")]
         [AcceptableValueRange(0f, 1f)]
         public static ConfigWrapper<float> Exposure { get; private set; }
-
-        [HarmonyPrefix]
-        [HarmonyPatch(typeof(SunLightInfo), nameof(SunLightInfo.Set))]
-        public static void SunLightInfoHook(SunLightInfo __instance, SunLightInfo.Info.Type type)
-        {
-            if (type == SunLightInfo.Info.Type.Night)
-            {
-                var amplifyColorEffect = Camera.main?.gameObject.GetComponent<AmplifyColorEffect>();
-                if (amplifyColorEffect != null)
-                    amplifyColorEffect.Exposure = Exposure.Value;
-            }
-
-            if (AdjustedList.Add(__instance))
-            {
-                foreach (var info in __instance.infos)
-                {
-                    if (info.type == SunLightInfo.Info.Type.Night)
-                    {
-                        if (UseFog.Value)
-                        {
-                            info.fogColor = SubtractColor(info.fogColor, 0.1f);
-                            info.fogStart = 1;
-                            info.fogEnd = 50;
-                        }
-
-                        //bug does this do anything?
-                        info.color = SubtractColor(info.color, 0.2f);
-                        info.intensity = info.intensity / 2f;
-                    }
-                }
-            }
-        }
-
+        
         private static void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1)
         {
-            if (arg1 == LoadSceneMode.Single)
-                AdjustedList.Clear();
+            var proc = FindObjectOfType<HSceneProc>();
+
+            if (proc == null || !proc.dataH.isFreeH) return;
+
+            var time = (SunLightInfo.Info.Type)proc.dataH.timezoneFreeH;
+            if (time != SunLightInfo.Info.Type.Night) return;
+
+            var allowedMaps = new[] { 0, 3, 4, 8, 9, 11, 15, 16, 17, 18, 20, 21, 22, 26, 28, 31, 32, 33, 34, 36, 37, 38, 45, 46, 47 };
+            if (!allowedMaps.Contains(proc.dataH.mapNoFreeH)) return;
+
+            var amplifyColorEffect = Camera.main?.gameObject.GetComponent<AmplifyColorEffect>();
+            if (amplifyColorEffect != null)
+                amplifyColorEffect.Exposure = Exposure.Value;
+
+            var lightInfo = FindObjectOfType<SunLightInfo>();
+
+            foreach (var info in lightInfo.infos)
+            {
+                if (info.type != SunLightInfo.Info.Type.Night) continue;
+
+                if (UseFog.Value)
+                {
+                    info.fogColor = SubtractColor(info.fogColor, 0.1f);
+                    info.fogStart = 1;
+                    info.fogEnd = 50;
+                }
+
+                //bug does this do anything?
+                info.color = SubtractColor(info.color, 0.2f);
+                info.intensity = info.intensity / 2f;
+            }
         }
 
         private void Start()
         {
             UseFog = new ConfigWrapper<bool>("UseFog", this);
             Exposure = new ConfigWrapper<float>("NightExposure", this, 0.3f);
-            
+
             HarmonyInstance.Create(GUID).PatchAll(typeof(NightDarkener));
             SceneManager.sceneLoaded += SceneManager_sceneLoaded;
         }
