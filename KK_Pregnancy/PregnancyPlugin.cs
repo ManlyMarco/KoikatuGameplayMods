09901ca17c3da09c8211b2f9b8ae03a94d1ee6e1
MarC0 <dummyaccx@gmail.com>
2019-06-23 21:17:44 +0200

    Added pregnancy mod stub

---
+++ KK_Pregnancy/PregnancyPlugin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BepInEx;
+
+namespace KK_Pregnancy
+{
+    [BepInPlugin(GUID, GUID, Version)]
+    [BepInDependency(KKABMX.Core.KKABMX_Core.GUID)]
+    [BepInDependency(KKAPI.KoikatuAPI.GUID)]
+    public class PregnancyPlugin : BaseUnityPlugin
+    {
+        public const string GUID = "KK_Pregnancy";
+        internal const string Version = "0.1";
+
+        private void Awake()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void Start()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void Update()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
