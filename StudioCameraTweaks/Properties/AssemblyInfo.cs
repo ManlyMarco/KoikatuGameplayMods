f718c2cd893d75acc611e57de54b1194418a0a58
MarC0 <dummyaccx@gmail.com>
2020-12-05 20:53:56 +0100

    Add StudioCameraTweaks

---
+++ StudioCameraTweaks/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using StudioCameraTweaks;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Studio Camera Tweaks")]
+[assembly: AssemblyDescription("Tweaks to camera items in studio")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("https://github.com/ManlyMarco/KoikatuGameplayMods")]
+[assembly: AssemblyProduct("StudioCameraTweaks")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1ca4932c-0b11-40cf-b906-23ccc52ad7e1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion(StudioCameraTweaksPlugin.Version)]
---
+++ StudioCameraTweaks/StudioCameraTweaksPlugin.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx;
+using HarmonyLib;
+using Studio;
+using UnityEngine;
+
+namespace StudioCameraTweaks
+{
+    [BepInPlugin(GUID, "Studio Camera Tweaks", Version)]
+    public class StudioCameraTweaksPlugin : BaseUnityPlugin
+    {
+        public const string GUID = "StudioCameraTweaks";
+        public const string Version = "1.0";
+
+        private void Awake()
+        {
+            Harmony.CreateAndPatchAll(typeof(StudioCameraTweaksPlugin));
+        }
+        
+        private static OCICamera _lastCamera;
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(AddObjectCamera), nameof(AddObjectCamera.Add))]
+        public static void AddCameraHook1(OCICamera __result)
+        {
+            _lastCamera = __result;
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(WorkspaceCtrl), "OnClickCamera")]
+        public static void AddCameraHook2()
+        {
+            var changeAmount = _lastCamera.objectInfo.changeAmount;
+            var camera = Camera.main.transform;
+            changeAmount.pos = camera.position;
+            changeAmount.rot = camera.rotation.eulerAngles;
+        }
+    }
+}
