441f88c06797a80e601d7e34b2bf38d1a2418528
MarC0 <dummyaccx@gmail.com>
2021-04-17 23:50:48 +0200

    Crest version up to 1.1

--- KK_LewdCrestX/LewdCrestXPlugin.cs
+++ KK_LewdCrestX/LewdCrestXPlugin.cs
@@ -33,7 +33,7 @@ namespace KK_LewdCrestX
     public partial class LewdCrestXPlugin : BaseUnityPlugin
     {
         public const string GUID = "LewdCrestX";
-        public const string Version = "1.0.2";
+        public const string Version = "1.1";
 
         public static Dictionary<CrestType, CrestInfo> CrestInfos { get; } = new Dictionary<CrestType, CrestInfo>();
 
