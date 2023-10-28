bc2c38a8accc0a71af5c719b96aa04f270906de3
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2023-10-28 12:03:31 +0200

    [Crest] Add unity project for making crest asset bundle

---
+++ src/Core_LewdCrestX/CrestsUnityProject/Assets/Editor/ExportAssetbundles.cs
@@ -0,0 +1,16 @@
+using UnityEditor;
+using System.IO;
+
+public class CreateAssetBundles
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        string assetBundleDirectory = Path.GetFullPath("Assets/AssetBundles");
+        if (!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.StandaloneWindows64);
+    }
+}
