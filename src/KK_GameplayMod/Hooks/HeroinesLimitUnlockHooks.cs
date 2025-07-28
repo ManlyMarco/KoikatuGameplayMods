62bc68af159bc189d2d6a5f090a6b7c5f9526d04
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2025-07-29 00:20:25 +0200

    Increase max additional chara slots to 1500

--- src/KK_GameplayMod/Hooks/HeroinesLimitUnlockHooks.cs
+++ src/KK_GameplayMod/Hooks/HeroinesLimitUnlockHooks.cs
@@ -15,7 +15,7 @@ namespace KoikatuGameplayMod
             if (KoikatuAPI.IsVR())
                 return false;
 
-            _addHeroins = config.Bind(KoikatuGameplayMod.ConfCatMainGame, "Increase max number of character slots", 0, new ConfigDescription("Maximum number of heroines in each location will be increased by this amount. You have to reopen the roster window after changing this setting.", new AcceptableValueRange<int>(0, 500)));
+            _addHeroins = config.Bind(KoikatuGameplayMod.ConfCatMainGame, "Increase max number of character slots", 0, new ConfigDescription("Maximum number of heroines in each location will be increased by this amount. You have to reopen the roster window after changing this setting.", new AcceptableValueRange<int>(0, 1500)));
 
             instance.PatchAll(typeof(HeroinesLimitUnlockHooks));
             return true;
