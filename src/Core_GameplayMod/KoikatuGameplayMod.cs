e5605b90f2b245c23dd3f399829678fd5606fbf0
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2025-09-13 19:17:13 +0200

    [GM] Version up to 2.4

--- src/Core_GameplayMod/KoikatuGameplayMod.cs
+++ src/Core_GameplayMod/KoikatuGameplayMod.cs
@@ -21,7 +21,7 @@ namespace KoikatuGameplayMod
     public class KoikatuGameplayMod : BaseUnityPlugin
     {
         public const string GUID = "marco-gameplaymod";
-        public const string Version = "2.3";
+        public const string Version = "2.4";
 
         internal const string ConfCatMainGame = "Main game";
         internal const string ConfCatHScene = "H Scene tweaks";
